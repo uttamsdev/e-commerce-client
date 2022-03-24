9443f2465f968b68a917670a0a1a5d293acfbb9e
UttamSaha <uttamsaha982@gmail.com>
2022-03-24 09:35:27 +0600

    header added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                <h1>Product container</h1>
+            </div>
+
+            <div className="cart-container">
+                <h1>Order sumary</h1>
+            </div>
+        </div>
+    )
+}
+export default Shop;
